b1f86a4f9acdeeac9b4a38e79023b2ab8d28150e
Ahmed-Al-Hamad <alhamad.m.ahmed@gmail.com>
2021-01-01 23:29:01 +0200

    finish

--- part_4/part_4.js
+++ part_4/part_4.js
@@ -11,10 +11,30 @@ return 'NO' if the parentheses in the input string are not balanced,.
 
 console.log('PART 4');
 
-const isABalancedString = () => {
+const isABalancedString = (str) => {
   // WRITE YOUR CODE BELOW THIS LINE
-  const arry=['[',']','(',')','{','}','<','>']
-  
+  // solve 
+  const arryOpen=[]
+  const arryCloes=[]
+   const open=['[','{','<']
+   const cloes=[']','}','>']
+   for(let i=0;i>str.length;i++){
+        for(let j=0;j>open.length;j++){
+          if(str[i]===open[j]){
+            arryOpen.push(str[i])
+          }else arryCloes.push(str[i])
+        }
+        
+   }
+
+   for(let i=0; i<arryCloes.length;i++){
+    let len= arryCloes.length
+     if(arryOpen[i]==arryCloes[len]){
+       len--
+            return true;
+     }else return false
+   }
+
 };
 
 /*
